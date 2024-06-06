cf5adbc556d085a8a4f6d18cd13e7bb4b6023a0b
EomSanghan <138652342+EomSanghan@users.noreply.github.com>
2024-06-07 07:13:13 +0900

    Update cal_userapp.py

--- cal_userapp.py
+++ cal_userapp.py
@@ -24,28 +24,28 @@ class cal_UserApp(tk.Toplevel):
         style.configure('Treeview.Heading', font=('Helvetica', 16, 'bold'))
         style.configure('Treeview', rowheight=25)
 
-        title_label = ttk.Label(self, text="콘서트 일정표", font=('Helvetica', 22, 'bold'))
+        title_label = ttk.Label(self, text="Peroformance Schedule", font=('Helvetica', 22, 'bold'))
         title_label.pack(pady=20)
 
-        days = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"]
+        days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
         day_frame = ttk.Frame(self)
         day_frame.pack()
 
         for i, day in enumerate(days):
-            label = ttk.Label(day_frame, text=day, borderwidth=1, relief="solid", width=15, anchor='center', font=('Helvetica', 14, 'bold'))
+            label = ttk.Label(day_frame, text=day, borderwidth=1, relief="solid", width=10, anchor='center', font=('Helvetica', 14, 'bold'))
             label.grid(row=0, column=i)
 
         cal_frame = ttk.Frame(self)
         cal_frame.pack()
 
         for day in range(1, 32):
-            btn = tk.Button(cal_frame, text=str(day), width=10, height=5)
+            btn = tk.Button(cal_frame, text=str(day), width=13, height=5)
             btn.grid(row=(day + 6) // 7, column=(day + 6) % 7, padx=5, pady=5, sticky="nsew")
             btn.bind("<Enter>", lambda e, b=btn: b.config(bg="lightgray"))
             btn.bind("<Leave>", lambda e, b=btn: b.config(bg="SystemButtonFace"))
             btn.config(command=lambda d=day: self.show_timetable(d))
 
-        back_button = ttk.Button(self, text="뒤로 가기", command=self.main_app.show_user_buttons)
+        back_button = ttk.Button(self, text="return", command=self.main_app.show_user_buttons)
         back_button.pack(pady=10)
 
     def show_timetable(self, day):
@@ -56,8 +56,8 @@ class cal_UserApp(tk.Toplevel):
         back_btn.pack(pady=10)
 
         tree = ttk.Treeview(self, columns=("Time", "Name"), show="headings")
-        tree.heading("Time", text="시간")
-        tree.heading("Name", text="이름")
+        tree.heading("Time", text="Time")
+        tree.heading("Name", text="Name")
 
         tree_style = ttk.Style()
         tree_style.configure('Treeview', font=('Helvetica', 15))
